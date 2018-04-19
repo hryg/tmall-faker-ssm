874a34e561e151ae9b140dc162d085e5bda20c66
Henry Heng <hengrui888@gmail.com>
2018-04-19 22:25:32 +0800

    创建数据库配置文件、日志配置文件、应用配置文件

--- src/main/java/com/hryg/tmall/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/hryg/tmall/service/impl/CategoryServiceImpl.java
@@ -4,9 +4,11 @@ import com.hryg.tmall.mapper.CategoryMapper;
 import com.hryg.tmall.pojo.Category;
 import com.hryg.tmall.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
     @Autowired
     CategoryMapper categoryMapper;
